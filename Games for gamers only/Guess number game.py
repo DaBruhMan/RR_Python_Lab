6271903108462148c6107edf0ea4538544697960
HollowKnight-git <rishikcl@gmail.com>
2020-03-17 19:50:01 -0700

    1.0

---
+++ Games for gamers only/Guess number game.py
@@ -0,0 +1,22 @@
+print (" guess a number between 1-10 you have 4 guesses")
+
+secret_number = 6
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+guess= float(input("Enter guess:"))
+
+while guess != secret_number and not (out_of_guesses):
+    if guess_count<guess_limit:
+        guess = float(input("Enter guess:"))
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+        print("Ha you are bad get a life you BUM")
+else:
+        print(" You are goated on the sticks WOW")
+        
+print("bye")
+myFinalResponse =(input("Enter response:"))

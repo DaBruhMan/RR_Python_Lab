d86b79a6fd8f0ce8258b49df0097a9f79294e64d
DaBruhMan <62317963+DaBruhMan@users.noreply.github.com>
2020-03-18 20:17:12 -0700

    ewojrfniaernfawjerf

---
+++ Games for gamers only/.idea/Weapom Attribute Base.py
@@ -0,0 +1,6 @@
+from Weapon_Class_Type import weapon
+
+(Stick, 10, 0, 0, 0, 0, 0)
+
+
+
---
+++ Games for gamers only/.idea/Weapon Class Type.py
@@ -0,0 +1,7 @@
+class weapon:
+    def __init__(self, damage, stunning, bleeding, burning, poisoned, frozen):
+        self.damage = damage
+        self.stunning = stunning
+        self.bleeding = bleeding
+        self.burning = burning
+        self.poisoned = frozen
